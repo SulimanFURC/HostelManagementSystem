21882efd10547dff6b913850ad267591f85828fa
SulimanFURC <Sullaimaan@gmail.com>
2025-06-17 00:28:23 +0500

    animation added in notification component ts to fix the console error

--- src/app/shared/notification/notification.component.ts
+++ src/app/shared/notification/notification.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { NotificationService, NotificationData } from '../../Services/notification.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
-  styleUrls: ['./notification.component.scss']
+  styleUrls: ['./notification.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({ transform: 'translateX(0)' })),
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('300ms ease-in')
+      ]),
+      transition(':leave', [
+        animate('300ms ease-out', style({ transform: 'translateX(100%)' }))
+      ])
+    ])
+  ]
 })
 export class NotificationComponent implements OnInit {
   notifications: NotificationData[] = [];
